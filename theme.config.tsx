19024e1e5f241c51617713493bf39aaee6e19412
Hari <harikrishkk@gmail.com>
2024-11-03 22:01:48 -0500

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  editLink: {
+    text: '',
+  },
+  feedback: {
+    labels: '',
+    content: '',
+  },
+  logo: <span>Next JS 14 Full Docs</span>,
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Made with ❤️ by Hari',
+  },
+  toc: {
+    float: false,
+  },
+};
+
+export default config;
